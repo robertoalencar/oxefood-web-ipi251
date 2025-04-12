203375387b1b785ec0353c20f2a1570c9f53735a
Roberto Alencar <rroberto.alencar@gmail.com>
2025-04-12 14:46:00 -0300

    enviando arquivos do projeto

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
+import { Segment } from 'semantic-ui-react';
 import './App.css';
-import Home from './views/home/Home';
+import FormCliente from './views/cliente/FormCliente';
 
 function App() {
   return (
     <div className="App">
       
-      <Home />
+      <FormCliente />
+
+      <div style={{marginTop: '6%'}}>
+        <Segment vertical color='grey' size='tiny' textAlign='center'>
+          &copy; 2023 - Projeto WEB III - IFPE Jaboatão dos Guararapes
+        </Segment>
+      </div>
 
     </div>
   );
